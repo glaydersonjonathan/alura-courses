de8f31d191a414fac8f9d9b2f6f4302ae3f34868
Glayderson J Nunes <glaydersonjonathan@gmail.com>
2017-08-16 17:17:22 -0300

    Adding a filter

--- gerenciador/src/main/java/br/com/alura/gerenciador/web/FiltroDeAuditoria.java
+++ gerenciador/src/main/java/br/com/alura/gerenciador/web/FiltroDeAuditoria.java
@@ -9,6 +9,7 @@ import javax.servlet.ServletException;
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
 import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
 
 @WebFilter(urlPatterns = "/*")
@@ -23,8 +24,29 @@ public class FiltroDeAuditoria implements Filter {
 			throws IOException, ServletException {
 
 		HttpServletRequest req = (HttpServletRequest) request;
-		System.out.println("Usuário acessando a URI: " + req.getRequestURI());
+		Cookie cookie = getUsuario(req);
+		String usuario = "<deslogado>";
+		
+		if (cookie != null)
+			usuario = cookie.getValue();
+		
+		System.out.println("Usuário: " + usuario + " acessando a URI: " + req.getRequestURI());
+		
 		chain.doFilter(request, response);
+	}
+
+	private Cookie getUsuario(HttpServletRequest req) {
+		Cookie[] cookies = req.getCookies();
+		
+		if (cookies == null)
+			return null;
+
+		for (Cookie cookie : cookies) {
+			if (cookie.getName().equals("usuario.logado")) {
+				return cookie;
+			}
+		}
+		return null;
 	}
 
 	@Override
