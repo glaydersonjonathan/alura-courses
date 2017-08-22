47f1003cb1eae800ceb58a064dae6f3a1f9342cf
Glayderson J Nunes <glaydersonjonathan@gmail.com>
2017-08-22 17:32:31 -0300

    Testing how to forward a request

--- gerenciador/src/main/java/br/com/alura/gerenciador/web/Logout.java
+++ gerenciador/src/main/java/br/com/alura/gerenciador/web/Logout.java
@@ -1,8 +1,8 @@
 package br.com.alura.gerenciador.web;
 
 import java.io.IOException;
-import java.io.PrintWriter;
 
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -19,11 +19,12 @@ public class Logout extends HttpServlet {
 	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 
 		HttpSession session = req.getSession();
-		
-		session.removeAttribute("usuario.logado");
+		session.removeAttribute("usuario.logado"); 
 
-		PrintWriter writer = resp.getWriter();
-		writer.println("<html><body>Logout efetuado</body></html>");
+		// resp.sendRedirect("/logout.html");
+		
+		RequestDispatcher dispatcher = req.getRequestDispatcher("/WEB-INF/paginas/logout.html");
+		dispatcher.forward(req, resp);
 	}
 
 }
